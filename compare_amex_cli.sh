0f99b25a1f1fb4bbbd44e0783debe8a897bfd8b3
bertcornelissen <b.cornelissen@xs4all.nl>
2026-02-16 19:23:13 +0100

    unix and windows  script split-up

--- compare_amex_cli.sh
+++ compare_amex_cli.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env bash
-# Convenience script for running ISO 8583 message comparison CLI
+# Convenience script for Unix running ISO 8583 message comparison CLI
 # Usage: ./compare_amex_cli.sh file1.txt file2.txt [options]
 
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "$(pwd)"
 
-# Detect the operating system and activate the virtual environment
-if [[ "$(lsb_release -is 2>/dev/null)" == "Ubuntu" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "Linux" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "Darwin" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "CYGWIN"* || "$(uname)" == "MINGW"* || "$(uname)" == "MSYS"* ]]; then
-    .venv\Scripts\activate
-else
-    echo "Unsupported OS. Exiting."
-    exit 1
-fi
+source .venv/bin/activat
 
 python3 compare_amex_cli.py "$@"
--- compare_amex_gui.sh
+++ compare_amex_gui.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
-# Convenience script for running ISO 8583 message comparison GUI
+# Convenience script for Unix running ISO 8583 message comparison GUI
 # Usage: ./compare_amex_gui.sh 
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Detect the operating system and activate the virtual environment
-if [[ "$(lsb_release -is 2>/dev/null)" == "Ubuntu" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "Linux" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "Darwin" ]]; then
-    source .venv/bin/activate
-elif [[ "$(uname)" == "CYGWIN"* || "$(uname)" == "MINGW"* || "$(uname)" == "MSYS"* ]]; then
-    .venv\Scripts\activate
-else
-    echo "Unsupported OS. Exiting."
-    exit 1
-fi
+source .venv/bin/activate
 
 streamlit run compare_amex_gui.py "$@"
